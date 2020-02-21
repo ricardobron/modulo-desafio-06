a16d71dfe0f1ad14999d71e561b61bcddb0d3faf
Ricardo <ricardoslv688@gmail.com>
2020-02-21 14:28:32 +0000

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+
+import './config/ReactotronConfig';
+
+import Routes from './routes';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar
+        // hidden={false} aparece
+        barStyle="ligh-content"
+        backgroundColor="#7159c1"
+      />
+      <Routes />
+    </>
+  );
+}
